13f90860b852a2dae05db72e4d83d199f51fb566
Sahil Sharma <officialsahil548k@gmail.com>
2026-07-11 22:44:59 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/connectDB.js";
+dotenv.config();
+
+
+const PORT = process.env.PORT || 6000;
+
+const app = express();
+
+
+
+app.listen(PORT, () => { 
+    console.log(`server is running on localhost:${PORT}`); 
+    connectDb(); 
+});
+
+app.get('/', (req, res) => {
+    res.send("Server running");
+});
