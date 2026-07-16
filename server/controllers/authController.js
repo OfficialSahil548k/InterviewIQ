90b702775918722f45d8a57a31727d4e2538a451
Sahil Sharma <officialsahil548k@gmail.com>
2026-07-17 01:19:39 +0530

    completed authentication setup using JWT

---
+++ server/controllers/authController.js
@@ -0,0 +1,45 @@
+import genToken from "../config/token.js";
+import User from "../models/userModel.js";
+
+export const googleAuth = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            user = await User.create({
+                name,
+                email
+            });
+        }
+
+        let token = genToken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "Lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const logOut = (req, res) => {
+    try {
+        res.clearCookie("token", {
+            httpOnly: true,
+            sameSite: "Lax",
+            secure: false
+        });
+        return res.status(200).json({ message: "logout successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/connectDB.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/authRoutes.js";
 dotenv.config();
-
-
-const PORT = process.env.PORT || 6000;
 
 const app = express();
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => { 
     console.log(`server is running on localhost:${PORT}`); 
     connectDb(); 
-});
-
-app.get('/', (req, res) => {
-    res.send('server is running...');
 });
