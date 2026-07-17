49bc20b587369eaadff4b46769818db017e643f7
Sahil Sharma <officialsahil548k@gmail.com>
2026-07-18 01:35:06 +0530

    set up for redux in the curren-user status is completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,34 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setUserData } from './redux/userSlice';
 
 export const ServerURL = "http://localhost:8000";
 
 const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(
+        `${ServerURL}/api/user/current-user`,
+        { withCredentials: true }
+      );
+
+      dispatch(setUserData(data));
+    } catch (error) {
+      dispatch(setUserData(null));
+      console.error(error);
+    }
+  };
+
+  getUser();
+}, [dispatch]);
   return (
     <div>
       <Routes>
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -6,21 +6,36 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../utils/firebase.js";
 import { ServerURL } from "../App.jsx";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../redux/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleGoogleAuth = async () => {
     try {
-      const res = await signInWithPopup( auth, provider);
-      let user = res.user;
-      let name = user.displayName;
-      let email = user.email;
-      const result = await axios.post(ServerURL + "/api/auth/google", {name, email}, {withCredentials:true});
-      console.log(result.data);
+      const res = await signInWithPopup(auth, provider);
+
+      const { displayName: name, email } = res.user;
+
+      const result = await axios.post(
+        ServerURL + "/api/auth/google",
+        { name, email },
+        { withCredentials: true }
+      );
+
+      // Update Redux state
+      dispatch(setUserData(result.data));
+
+     // navigate("/");
+
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
   return (
     <div className="w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20">
       <motion.div
@@ -38,22 +53,26 @@ const Auth = () => {
 
         <h1 className="text-2xl md:text-3xl font-semibold text-center leading-snug mb-4">
           Continue with{" "}
-          <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex item-center gap-2 ">
-            <IoSparkles size={16} className="mt-3" />
-            Ai Smart Interview
+          <span className="bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2">
+            <IoSparkles size={16} />
+            AI Smart Interview
           </span>
         </h1>
-        <p className="text-gray-500 text-center text-sm md:text-base leading-relaxed mb-8 ">
+
+        <p className="text-gray-500 text-center text-sm md:text-base leading-relaxed mb-8">
           Sign in to start AI-powered mock interviews, track your progress, and
-          progress, and unlock detailed performance insights.
+          unlock detailed performance insights.
         </p>
 
         <motion.button
           onClick={handleGoogleAuth}
-          whileHover={{ opacity : 0.9 , scale: 1.1 }}
-          whileTap={{ opacity : 1, scale: 0.95 }}
-          className='w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md'
-        ><FcGoogle size={20}/>Sign-in with Google</motion.button>
+          whileHover={{ opacity: 0.9, scale: 1.05 }}
+          whileTap={{ opacity: 1, scale: 0.95 }}
+          className="w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md"
+        >
+          <FcGoogle size={20} />
+          Sign in with Google
+        </motion.button>
       </motion.div>
     </div>
   );
