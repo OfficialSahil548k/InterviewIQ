d8816da07aaccf5372937a1ca9e7fff30048ce9b
Sahil Sharma <officialsahil548k@gmail.com>
2026-07-25 21:50:24 +0530

    Setup for interview settings and resume upload & Analysis service completed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import interviewRouter from "./routes/interviewRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/interview",interviewRouter);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => { 
